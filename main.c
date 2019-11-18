707be899c5aea634c0c03a793af25f9ee8de4feb
Cheng-Yi-Lin <s10482039@gm2.nutn.edu.tw>
2019-11-18 22:53:37 +0800

    function pointer

--- main.c
+++ main.c
@@ -5,19 +5,31 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
-int main()
+
+int (*p[4])(int, int);
+
+int main(int argc, char* argv[])
 {
+	
+	p[0]=plus;
+	p[1]=minus;
+	p[2]=multiply;
+	p[3]=divided;	
+
+	int val;
 	int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
+	
 	//IF ELSE IF
 	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
+		val = (*p[0])(a, c);		
 	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
+		val = (*p[1])(a, c);
 	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
+		val = (*p[2])(a, c);
 	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+		val = (*p[3])(a, c);
+	printf("%d %c %d = %d\n", a, b, c, val);
 }
